ebb752bc1ac0036bfeefe808b8ffadb71351eaa4
Bruno <brunoomiguel10@hotmail.com>
2023-12-03 15:01:41 +0000

    Movement functions done

--- src/key_handler.c
+++ src/key_handler.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   key_handler.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: brpereir <brpereir@student.42.fr>          +#+  +:+       +#+        */
+/*   By: brunolopes <brunolopes@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/11 19:34:47 by brpereir          #+#    #+#             */
-/*   Updated: 2023/11/30 18:35:23 by brpereir         ###   ########.fr       */
+/*   Updated: 2023/12/03 14:59:25 by brunolopes       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,28 @@
 int	close_window(void)
 {
 	exit(EXIT_SUCCESS);
+}
+
+static void move_check(t_game **game)
+{
+	static int	flag;
+
+	if ((*game)->map[(*game)->player.curr_y][(*game)->player.curr_x] == 'C')
+		(*game)->collectibles--;
+	if ((*game)->map[(*game)->player.curr_y][(*game)->player.curr_x] == 'E' && !(*game)->collectibles){
+		printf("You won");
+		exit(1);
+	}
+	if ((*game)->map[(*game)->player.curr_y][(*game)->player.curr_x] == 'E')
+		flag = 1;
+	if (flag == 2){
+		(*game)->map[(*game)->player.prev_y][(*game)->player.prev_x] = 'E';
+		flag = 0;
+	} else 
+		(*game)->map[(*game)->player.prev_y][(*game)->player.prev_x] = '0';
+	if (flag == 1)
+		flag = 2;
+	(*game)->map[(*game)->player.curr_y][(*game)->player.curr_x] = 'P';
 }
 
 int key_handler(int keycode, t_game **game)
@@ -36,9 +58,8 @@ int key_handler(int keycode, t_game **game)
 		(*game)->player.curr_y = (*game)->player.prev_y;
 		(*game)->player.curr_x = (*game)->player.prev_x;
 		return (0);
-	}
-	(*game)->map[(*game)->player.prev_y][(*game)->player.prev_x] = '0';
-	(*game)->map[(*game)->player.curr_y][(*game)->player.curr_x] = 'P';
+	} 
+	move_check(game);
 	put_image(game);
 	return (0);
 }
